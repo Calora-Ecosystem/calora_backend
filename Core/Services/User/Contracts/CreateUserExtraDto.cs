f64c6f4e71dfb93ed86f46c32d4d901b8e93049d
Ramadan1011 <shukhratovich75@gmail.com>
2025-07-03 09:17:43 +0500

    feat: create dtos

---
+++ Core/Services/User/Contracts/CreateUserExtraDto.cs
@@ -0,0 +1,20 @@
+﻿using Core.Enums;
+
+namespace Core.Services.User.Contracts;
+
+public class CreateUserExtraDto
+{  
+    public string Name { get; set; } = null!;
+    public double Weight { get; set; }
+    public double Height { get; set; }
+    public double Bmi { get; set; }
+    public EnumGender Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string? Photo { get; set; }
+    public List<long> PurposeIds { get; set; } = new();
+}
+
+public class UpdateUserExtraDto : CreateUserExtraDto
+{
+    public long Id { get; set; }
+}
---
+++ Core/Services/User/Contracts/CreateUserNormDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Enums;
+
+namespace Core.Services.User.Contracts;
+
+public class CreateUserNormDto
+{
+    public EnumMetrics Metric { get; set; }
+    public double Value { get; set; }
+}
+
+public class UpdateUserNormDto
+{
+    public long Value { get; set; }
+}
