4b43b36c67e82245e60a0d1a80aa10bd5bac7d52
0605AbMu <0605AbMu@gmail.com>
2025-07-05 14:26:07 +0500

    Add Injectable attribute to NotificationService class

--- Core/Services/Notification/NotificationService.cs
+++ Core/Services/Notification/NotificationService.cs
@@ -1,8 +1,10 @@
+using BRB.Core.EF.Attributes;
 using Core.Brokers.DbContext;
 using Core.Brokers.EmailBroker;
 
 namespace Core.Services.Notification;
 
+[Injectable]
 public partial class NotificationService(EmailClient emailClient, AppDbContext dbContext)
 {
 }
