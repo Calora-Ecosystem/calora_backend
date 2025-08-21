27e19c774c2ce6fb691f867902ac516a2d22fae7
Abdumannon <0605AbMu@gmail.com>
2025-08-21 19:00:03 +0500

    update OtpSign message template with new app name

--- Core/Constants/MessageTemplates.cs
+++ Core/Constants/MessageTemplates.cs
@@ -2,7 +2,7 @@
 
 public static class MessageTemplates
 {
-     public const string OtpSign = "Zingo ilovasiga kirish uchun bir martalik kod: {0}";
+     public const string OtpSign = "Calora ilovasiga kirish uchun bir martalik kod: {0}";
      
      public static string MakeMessage(string template, params object?[] args) => string.Format(template, args);
 }
