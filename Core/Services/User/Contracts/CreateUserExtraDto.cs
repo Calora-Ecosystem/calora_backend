aaa74483a3e9d8cb512ef244a7f9acfa04ca0c48
0605AbMu <0605AbMu@gmail.com>
2025-10-14 16:34:50 +0500

    improve CreateUserExtraDto validation and refine BMI calculation logic

--- Core/Services/User/Contracts/CreateUserExtraDto.cs
+++ Core/Services/User/Contracts/CreateUserExtraDto.cs
@@ -1,13 +1,15 @@
-﻿using Core.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Enums;
 
 namespace Core.Services.User.Contracts;
 
 public class CreateUserExtraDto
 {
     public string Name { get; set; } = null!;
+    [Range(20, int.MaxValue)]
     public double Weight { get; set; }
+    [Range(20, int.MaxValue)]
     public double Height { get; set; }
-    public double Bmi { get; set; }
     public EnumGender Gender { get; set; }
     public DateTime BirthDate { get; set; }
     public string? Photo { get; set; }
