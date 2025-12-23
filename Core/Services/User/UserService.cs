84d897f187b1f02d906073a40dad397ec86b7829
0605AbMu <0605AbMu@gmail.com>
2025-12-23 23:02:38 +0500

    round bmi

--- Core/Services/User/UserService.cs
+++ Core/Services/User/UserService.cs
@@ -34,7 +34,8 @@ public class UserService(AppDbContext context)
     {
         var extra = await context.UserExtras
                         .Where(x => x.UserId == userId)
-                        .Select(x => new GetUserExtraDto(x.UserId, x.Weight, x.EntryWeight, x.Height, x.Bmi, x.Gender,
+                        .Select(x => new GetUserExtraDto(x.UserId, x.Weight, x.EntryWeight, x.Height,
+                            Math.Round(x.Bmi, 0), x.Gender,
                             x.BirthDate,
                             x.Photo, x.Name, x.ActivityLevel, x.Purpose))
                         .FirstOrDefaultAsync()
@@ -96,7 +97,7 @@ public class UserService(AppDbContext context)
                 Metric = EnumMetrics.Weight,
                 Value = dto.TargetWeight
             });
-            
+
             //Step
             await CreateOrUpdateNorm(userId, new CreateUserNormDto()
             {
@@ -258,7 +259,7 @@ public class UserService(AppDbContext context)
                     }), general => general.Metric, arg => arg.Metric,
                 (general, arg2) => new UserProgressSummaryDto
                 {
-                    Metric = general.Metric, Target = Math.Round(general.Value,0),
+                    Metric = general.Metric, Target = Math.Round(general.Value, 0),
                     Progress = !arg2.IsNullOrEmpty() ? arg2.First().Sum : 0
                 })
             .ToListAsync();
