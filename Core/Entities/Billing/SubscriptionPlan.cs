21b521d5ecac4285ba187044627b25d572d3e28e
0605AbMu <0605AbMu@gmail.com>
2025-12-23 15:04:19 +0500

    extend SubscriptionPlan to inherit from AuditableModelBase

--- Core/Entities/Billing/SubscriptionPlan.cs
+++ Core/Entities/Billing/SubscriptionPlan.cs
@@ -1,8 +1,9 @@
 ﻿using BRB.Core.Common.Models;
+using BRB.Core.Common.Models.Base;
 
 namespace Core.Entities.Billing;
 
-public class SubscriptionPlan
+public class SubscriptionPlan : AuditableModelBase<long>
 {
     public MultiLanguageField Name { get; set; } = null!;
     public decimal Fee { get; set; }
