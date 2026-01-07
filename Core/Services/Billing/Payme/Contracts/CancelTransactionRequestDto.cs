7bc76dbdcefa0f58919c474177cd40558de4383f
0605AbMu <0605AbMu@gmail.com>
2026-01-07 19:29:42 +0500

    feat: extend PaymeService with transaction management functionality
    - Added DTOs for creating, performing, checking, and canceling transactions in Payme.
    - Implemented transaction management methods in `PaymeService`.
    - Enhanced `ResponseErrors` with new error types for transaction handling.
    - Updated `OrderService` to use internal transaction creation for Payme.

---
+++ Core/Services/Billing/Payme/Contracts/CancelTransactionRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Services.Billing.Payme.Contracts;
+
+public class CancelTransactionRequestDto
+{
+    public string Id { get; set; } = null!;
+    public short Reason { get; set; }
+}
---
+++ Core/Services/Billing/Payme/Contracts/CreateTransactionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Services.Billing.Payme.Contracts;
+
+public class CreateTransactionDto
+{
+    public AccountBaseDto Account { get; set; } = null!;
+    public long Amount { get; set; }
+    public string Id { get; set; } = null!;
+    public long Time { get; set; }
+}
