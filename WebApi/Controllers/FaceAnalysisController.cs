42dd23195427937924bd47fbf6e38a3ef997bf93
0605AbMu <0605AbMu@gmail.com>
2026-01-20 15:16:16 +0500

    remove redundant feature limitation

--- WebApi/Controllers/FaceAnalysisController.cs
+++ WebApi/Controllers/FaceAnalysisController.cs
@@ -24,7 +24,7 @@ namespace WebApi.Controllers
         }
 
         [HttpPost("analyze")]
-        [RoleAuthorize(EnumRole.User, Plans = [EnumSPlans.Premium])]
+        [RoleAuthorize(EnumRole.User)]
         public async Task<WrapperGeneric<AnalyzeFaceDto>> Analyze([FromForm] UploadFileDto image)
         {
             var bytes = new byte[image.File.Length];
