d9623215fcaa27e1b0d98fb34db568ea579abd2e
0605AbMu <0605AbMu@gmail.com>
2026-02-05 16:06:48 +0500

    fix: add unique index for UserDaily on UserId, Date, and Metric to ensure data integrity

--- Core/Entities/Auth/UserDaily.cs
+++ Core/Entities/Auth/UserDaily.cs
@@ -3,6 +3,7 @@
 namespace Core.Entities.Auth;
 
 [Index(nameof(Date))]
+[Index(nameof(UserId), nameof(Date), nameof(Metric), IsUnique = true)]
 public class UserDaily : UserNormGeneral
 {
     public DateTime Date { get; set; }
