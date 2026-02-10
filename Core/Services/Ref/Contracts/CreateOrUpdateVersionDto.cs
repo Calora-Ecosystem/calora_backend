ee88a281548c857eefff692fcf9b7888d10bd0cf
0605AbMu <0605AbMu@gmail.com>
2026-02-10 17:21:11 +0500

    fix: add Versions table and related DTOs to support versioning functionality

---
+++ Core/Services/Ref/Contracts/CreateOrUpdateVersionDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Services.Ref.Contracts;
+
+public class CreateOrUpdateVersionDto
+{
+    public long? Id { get; set; }
+    public string? Key { get; set; } = null!;
+    public bool IsActive { get; set; }
+}
---
+++ WebApi/Controllers/Refs/VersionController.cs
@@ -0,0 +1,33 @@
+﻿using BRB.Core.Common.Models;
+using Core.Services.Ref;
+using Core.Services.Ref.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using ResultWrapper.Library;
+using Version = Core.Entities.Refs.Version;
+
+namespace WebApi.Controllers.Refs;
+
+[ApiController]
+[Route("versions")]
+public class VersionController(VersionService versionService) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType<WrapperGeneric<IEnumerable<Version>>>(200)]
+    public async Task<Wrapper> GetAll([FromQuery] DataQueryRequest q) => await versionService.GetAll(q);
+
+    [HttpGet("{version}")]
+    [ProducesResponseType<WrapperGeneric<CheckDto>>(200)]
+    public async Task<Wrapper> Check(string version) => (await versionService.Check(version), 200);
+
+    [HttpGet("latest")]
+    [ProducesResponseType<WrapperGeneric<Version>>(200)]
+    public async Task<Wrapper> GetLatestVersion() => (await versionService.GetLatestVersion(), 200);
+
+    [HttpPost]
+    public async Task<Wrapper> CreateOrUpdate([FromBody] CreateOrUpdateVersionDto dto)
+    {
+        await versionService.CreateOrUpdate(dto);
+
+        return 200;
+    }
+}
