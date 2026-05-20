fce46281346b5b577b986d18202325f9dd81a6b7
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2026-05-20 10:50:14 +0500

    update WorkoutService: revise computation retrieval logic to prioritize existing records and avoid redundant entity creation

--- Core/Services/Course/Workout/WorkoutService.cs
+++ Core/Services/Course/Workout/WorkoutService.cs
@@ -359,14 +359,15 @@ public class WorkoutService(AppDbContext dbContext)
 
             var computation = dto.Id.HasValue
                 ? await dbContext.Computations.GetByIdOrThrowsNotFoundException(dto.Id.Value)
-                : dbContext.Add(new Computation()
+                : await dbContext.Computations.FirstOrDefaultAsync(x => x.EntityId == dto.EntityId && x.Type == dto.Type && x.Level == dto.Activity && x.ComputationType == dto.ComputationType) ??
+                dbContext.Add(new Computation()
                 {
                     Type = dto.Type,
                     EntityId = dto.EntityId,
+                    Level = dto.Activity,
+                    ComputationType = dto.ComputationType
                 }).Entity;
 
-            computation.Level = dto.Activity;
-            computation.ComputationType = dto.ComputationType;
             computation.Value = dto.Value;
         }
 
